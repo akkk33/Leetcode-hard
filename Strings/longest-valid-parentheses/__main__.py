6fdd9fb897e2da25d5d5a742f2ce742f27c79307
akkk33 <akkk33@protonmail.com>
2018-11-06 08:38:21 +0200

    Not finished yet

--- Strings/longest-valid-parentheses/__main__.py
+++ Strings/longest-valid-parentheses/__main__.py
@@ -1,3 +1,20 @@
+"""
+URL: https://leetcode.com/problems/longest-valid-parentheses/
+Problem description:
+Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+Example 1:
+
+Input: "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()"
+Example 2:
+
+Input: ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()"
+
+"""
 class Solution:
     def longest_valid_parentheses(self, s):
         """
--- Strings/substring-with-concatenation-of-all-words/scratch.py
+++ Strings/substring-with-concatenation-of-all-words/scratch.py
@@ -1,14 +1,14 @@
 """
 https://stackoverflow.com/a/28870795/7301680
 """
-# from itertools import permutations
-#
-# stuff = ["Amr", "Helal", "Maged"]
-# perms = []
-# for i in range(1, len(stuff) + 1):
-#     for c in permutations(stuff, i):
-#         perms.append("".join(c))
-#
-# print(perms)
+from itertools import permutations
+
+stuff = ["Amr", "Helal", "Maged"]
+perms = []
+for i in range(1, len(stuff) + 1):
+    for c in permutations(stuff, i):
+        perms.append("".join(c))
+
+print(perms)
 
 print([x for x in range(2, 6, 1)])
