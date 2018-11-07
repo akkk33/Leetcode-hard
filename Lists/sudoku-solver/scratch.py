ecdd4f9b705ff771beecd7c8332e9e5f60be5867
akkk33 <akkk33@protonmail.com>
2018-11-07 08:32:36 +0200

    Beginning with Sudoku solver

---
+++ Lists/sudoku-solver/scratch.py
@@ -0,0 +1,11 @@
+"""
+References
+
+"""
+"""
+to understand it:
+we have 9 squares inside each one there are 9 numbers
+We'll call the top row of squares 'a': [a1,a2,a3]
+the middle row will be 'b': [b1,b2,b3]
+the bottom row will be 'c': [c1,c2,c3]
+"""
