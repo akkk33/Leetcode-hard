f0fac35a56100eccf660e1139d94c509d58e08c3
akkk33 <akkk33@protonmail.com>
2018-11-07 07:57:32 +0200

    Wrong answer when inputting ()(()) Can't find why it should be 6 while it's actually 4 only !!

--- Strings/longest-valid-parentheses/scratch.py
+++ Strings/longest-valid-parentheses/scratch.py
@@ -5,22 +5,33 @@ References
 
 
 class Solution:
-    def longestValidParentheses(self, s):
+    def longest_valid_parentheses(self, s):
         """
         :type s: str
         :rtype: int
         """
+        popen = 0
+        pclose = 0
         result = []
         if 0 <= s.find('(') < s.rfind(')'):
             s = s[s.find('('):s.rfind(')') + 1]
         else:
             s = ''
         for index, letter in enumerate(s):
-            if letter == ')':
-                if s[index - 1] == '(':
-                    result.append(2)
-        return sum(result)
+            if letter == '(':
+                if index != 0 and s[index - 1] != '(' and s[index-2] != '(':
+                    popen = 0
+                popen += 1
+            else:  # )
+                if s[index - 1] != ')' and s[index - 2] != ')':
+                    pclose = 0
+                pclose += 1
+                result.append(min(pclose, popen) * 2)
+        if len(result) > 0:
+            return max(result)
+        else:
+            return 0
 
 
 ex = Solution()
-print(ex.longestValidParentheses(")("))
+print(ex.longest_valid_parentheses("()(())"))
