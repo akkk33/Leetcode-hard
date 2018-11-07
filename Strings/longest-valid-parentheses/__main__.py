649081127eb61cf2dcc7f5b40250e42bc5d73e43
akkk33 <akkk33@protonmail.com>
2018-11-07 07:58:13 +0200

    Will attempt another approach

--- Strings/longest-valid-parentheses/__main__.py
+++ Strings/longest-valid-parentheses/__main__.py
@@ -20,6 +20,8 @@ but It never
 """
 
 
+
+
 class Solution:
     def longest_valid_parentheses(self, s):
         """
@@ -35,15 +37,19 @@ class Solution:
             s = ''
         for index, letter in enumerate(s):
             if letter == '(':
-                if index != 0 and s[index - 1] != '(':
+                if index != 0 and s[index - 1] != '(' and s[index-2] != '(':
                     popen = 0
                 popen += 1
             else:  # )
+                if s[index - 1] != ')' and s[index - 2] != ')':
+                    pclose = 0
                 pclose += 1
                 result.append(min(pclose, popen) * 2)
         if len(result) > 0:
             return max(result)
+        else:
+            return 0
 
 
 ex = Solution()
-print(ex.longest_valid_parentheses(")()())"))
+print(ex.longest_valid_parentheses("()(())"))
